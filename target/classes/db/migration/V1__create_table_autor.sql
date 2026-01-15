8fc6f7d2dccf13a4ca15b761b98c4e2f64e441d4
esteroliver <oliver.ester.2004@gmail.com>
2026-01-15 20:26:37 -0300

    :wastebasket: remove: removendo pasta target

--- target/classes/db/migration/V1__create_table_autor.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA biblioteca;
-
-CREATE TABLE biblioteca.autor(
-    id BIGINT PRIMARY KEY ,
-    nome VARCHAR NOT NULL
-);
--- target/classes/db/migration/V2__create_table_livro.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE biblioteca.livro(
-     id BIGINT PRIMARY KEY ,
-     titulo VARCHAR NOT NULL ,
-     num_paginas INTEGER NOT NULL ,
-     isbn VARCHAR NOT NULL ,
-     sinopse VARCHAR,
-     autor_id BIGINT references biblioteca.autor(id)
-);
